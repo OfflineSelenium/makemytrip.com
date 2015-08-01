0d3708905931e6d6cc2b9ad3db5817e44abbb205
ducthinh100286 <ducthinh_100286@yahoo.com>
2015-08-01 11:05:59 +0700

    Added 3 missing files from Thach

---
+++ src/main/java/makemytrip/page/objects/SearchPage.java
@@ -0,0 +1,60 @@
+package makemytrip.page.objects;
+
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.springframework.stereotype.Component;
+
+@Component
+public class SearchPage extends BasePage {
+
+    @FindBy(id = "from_city_data_box")
+    private WebElement txtSearch;
+
+    @FindBy(xpath = "//span[contains(@class,'night_number')]")
+    private WebElement cmbNight;
+
+    @FindBy(xpath = "//div[contains(@class,'first_room_div')]//div[contains(@class,'segmented_controls')]//a[2]")
+    private WebElement Adult;
+
+    @FindBy(xpath = "//div[contains(@class,'col-sm-3')]//div[contains(@class,'segmented_controls')]//a[2]")
+    private WebElement Children;
+
+    @FindBy(id = "hotels_submit")
+    private WebElement btnClickSearch;
+
+    public void load() {
+        loadPage("/hotels/");
+    }
+
+    public SearchPage enterSearchKeyword(String text) {
+        txtSearch.clear();
+        txtSearch.sendKeys(text);
+
+        //waiting element displayed
+        String xpath = "//div[contains(@class,'modify_ddn_fields')]//span[contains(@class,'tt-dropdown-menu')]";
+        webElementFinder.findElementByLocatorXPath(xpath).isDisplayed();
+
+        return this;
+    }
+
+    public SearchPage chooseNight(String night) {
+        cmbNight.sendKeys(night);
+        return this;
+    }
+
+    public SearchPage chooseAdult() {
+        Adult.click();
+        return this;
+    }
+
+    public SearchPage chooseChildren() {
+        Children.click();
+        return this;
+    }
+
+    public SearchResultsPage clickSearch() {
+        btnClickSearch.click();
+        return webPageFactory.loadSearchResultsPage();
+    }
+
+}
---
+++ src/main/java/makemytrip/page/objects/SearchResultsPage.java
@@ -0,0 +1,35 @@
+package makemytrip.page.objects;
+
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.springframework.stereotype.Component;
+
+@Component
+public class SearchResultsPage extends BasePage {
+
+    @FindBy(xpath = "//div[4]/p[2]/span")
+    private WebElement searchResultNighht;
+
+    @FindBy(xpath = "//div[5]/p[2]/span")
+    private WebElement searchResultRooms;
+
+    @FindBy(xpath = "//div[6]/p[2]/span")
+    private WebElement searchResultPeople;
+
+    public boolean shuoldSeeTextTitle(String text) {
+        return driver.getTitle().contains(text);
+    }
+
+    public boolean shouldSeeNightSearchResult(String text) {
+        return searchResultNighht.getText().equals(text);
+    }
+
+    public boolean shouldSeeRoomSearchResult(String text) {
+        return searchResultRooms.getText().contains(text);
+    }
+
+    public boolean shouldSeePeopleSearchResult(String text) {
+        return searchResultPeople.getText().equals(text);
+    }
+
+}
